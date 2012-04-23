bf88490336eed109ccdd06f785c1c02ea17645da
gerritvanaaken <gerrit@praegnanz.de>
2012-04-23 20:34:41 +0200

    Several updates, dropped Fancybox, added Zepto as option

--- js/domscript.js
+++ js/domscript.js
@@ -1,9 +1,5 @@
-function dom_init() {
+$(function(){
 
-  /* See http://fancybox.net/api for all options */
-  $("a.fancybox").fancybox({
-    'speedIn' : 600,
-    'speedOut' : 300
-  });
-  
-}
+	// Do jQuery or Zepto magic here
+
+});
